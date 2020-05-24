e64488a8b48bfbb4b5bb71c54c17ab3a716f4057
yang_going <2210744381@qq.com>
2020-05-24 22:48:09 +0800

    播放器页面

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,13 +1,20 @@
 let musiclist = []
 // 正在播放音乐的index
 let nowPlayingIndex = 0
+// 获取全局唯一背景音频管理器
+const backgroundAudioManager = wx.getBackgroundAudioManager()
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    picUrl: ''
+    picUrl: '',
+    isPlaying: false,
+    isLyricShow: false,
+    lyric: '',
+    isSame: false
   },
 
   /**
@@ -16,16 +23,129 @@ Page({
   onLoad: function (options) {
     nowPlayingIndex = options.index
     musiclist = wx.getStorageSync('musiclist')
-    this._loadMusicDetail()
+    this._loadMusicDetail(options.musicId)
   },
 
-  _loadMusicDetail() {
+  _loadMusicDetail(musicId) {
+    if (musicId === app.getPlayingMusicId()) {
+      this.setData({
+        isSame: true
+      })
+    } else {
+      this.setData({
+        isSame: false
+      })
+    }
+    if (!this.data.isSame) {
+      backgroundAudioManager.stop()
+    }
     let music = musiclist[nowPlayingIndex]
     wx.setNavigationBarTitle({
       title: music.name,
     })
     this.setData({
-      picUrl: music.al.picUrl
+      picUrl: music.al.picUrl,
+      isPlaying: false
+    })
+
+    app.setPlayingMusicId(musicId)
+
+    wx.showLoading({
+      title: '正在觅荷...',
+    })
+    wx.cloud.callFunction({
+      name: 'music',
+      data: {
+        $url: 'musicUrl',
+        musicId,
+        isPlaying: false
+      }
+    }).then(res => {
+      let result = JSON.parse(res.result)
+      if (result.data[0].url == null) {
+        wx.showToast({
+          title: '没会员没办法，卡碟放不了',
+        })
+        return
+      }
+      // 不是同一首歌曲
+      if (!this.data.isSame) {
+        backgroundAudioManager.src = result.data[0].url
+        backgroundAudioManager.title = music.name
+        backgroundAudioManager.coverImgUrl = music.al.picUrl
+        backgroundAudioManager.singer = music.ar[0].name
+        backgroundAudioManager.epname = music.al.name
+      }
+
+      this.setData({
+        isPlaying: true
+      })
+      wx.hideLoading()
+      // 加载歌词
+      wx.cloud.callFunction({
+        name: 'music',
+        data: {
+          musicId,
+          $url: 'lyric'
+        }
+      }).then(res => {
+        let lyric = '它可能是个纯音乐，所以没有歌词...'
+        const lrc = JSON.parse(res.result).lrc
+        if (lrc) {
+          lyric = lrc.lyric
+        }
+        this.setData({
+          lyric
+        })
+
+      })
+    })
+  },
+
+  togglePlaying() {
+    if (this.data.isPlaying) {
+      backgroundAudioManager.pause()
+    } else {
+      backgroundAudioManager.play()
+    }
+    this.setData({
+      isPlaying: !this.data.isPlaying
+    })
+  },
+
+  onPrev() {
+    nowPlayingIndex--
+    if (nowPlayingIndex < 0) {
+      nowPlayingIndex = musiclist.length - 1
+    }
+    this._loadMusicDetail(musiclist[nowPlayingIndex].id)
+  },
+  onNext() {
+    nowPlayingIndex++
+    if (nowPlayingIndex === musiclist.length) {
+      nowPlayingIndex = 0
+    }
+    this._loadMusicDetail(musiclist[nowPlayingIndex].id)
+  },
+
+  onChangeLyricShow() {
+    this.setData({
+      isLyricShow: !this.data.isLyricShow
+    })
+  },
+  timeUpdate(event) {
+    this.selectComponent('.lyric').update(event.detail.currentTime)
+  },
+
+  onPlay() {
+    this.setData({
+      isPlaying: true
+    })
+  },
+
+  onPause() {
+    this.setData({
+      isPlaying: false
     })
   },
   /**
